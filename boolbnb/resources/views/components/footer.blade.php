6b134492706da76320f454d4538e6c675ee7b833
B.C <benblog2000@gmail.com>
2021-06-16 15:49:06 +0200

    footer0.2 - debug

--- boolbnb/resources/views/components/footer.blade.php
+++ boolbnb/resources/views/components/footer.blade.php
@@ -9,37 +9,66 @@
                     <h5>Hello</h5>
                     @for ($i = 0; $i < 7; $i++)
                         <li>
-                            Lorem, ipsum dolor.
+                            <a href="#" class="links-ft">
+                                Lorem, ipsum dolor.
+                            </a>
                         </li>
                     @endfor
                 </ul>
+
+                <hr class="invisible">
     
                 <ul>
                     <h5>Hello</h5>
                     @for ($i = 0; $i < 7; $i++)
                         <li>
-                            Lorem, ipsum dolor.
+                            <a href="#" class="links-ft">
+                                Lorem, ipsum dolor.
+                            </a>
                         </li>
                     @endfor
                 </ul>
+
+                <hr class="invisible">
     
                 <ul>
                     <h5>Hello</h5>
                     @for ($i = 0; $i < 7; $i++)
                         <li>
-                            Lorem, ipsum dolor.
+                            <a href="#" class="links-ft">
+                                Lorem, ipsum dolor.
+                            </a>
                         </li>
                     @endfor
                 </ul>
+
+                <hr class="invisible">
     
                 <ul>
                     <h5>Hello</h5>
                     @for ($i = 0; $i < 7; $i++)
                         <li>
-                            Lorem, ipsum dolor.
+                            <a href="#" class="links-ft">
+                                Lorem, ipsum dolor.
+                            </a>
                         </li>
                     @endfor
                 </ul>
+
+                <hr class="invisible">
+            </div>
+
+            <hr>
+
+            <div class="row" id="last-row-footer">
+                <span>
+                    {{-- aggiungere simbolo commerciale al posto dell'asterisco --}}
+                    * 2021 Airbnb, Inc. · <a href="#" class="links-ft">Privacy</a> · <a href="#" class="links-ft">Termini</a> · <a href="#" class="links-ft">Mappa del sito</a> · <a href="#" class="links-ft">Dettagli dell'azienda</a>
+                </span>
+
+                <span>
+                    <i class="fas fa-globe"></i><a href="#" class="links-ft">Italinao (IT)</a>
+                </span>
             </div>
 
 
