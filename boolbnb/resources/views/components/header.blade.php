afc2fc02986250f0e0151729088e576a896a726e
Alessandro <aletibo@gmail.com>
2021-06-16 14:41:36 +0200

    Modifica header

--- boolbnb/resources/views/components/header.blade.php
+++ boolbnb/resources/views/components/header.blade.php
@@ -1,9 +1,33 @@
 <header>
-  <div class="container-fluid">
+  <div class="container-header">
 
-    <div class="row">
-      <h2>This is an amazing header</h2>
+    <div class="logo-header">
+      <img src="{{ asset('storage/images/logo.svg') }}" alt="">
     </div>
 
+    <div class="searchbar-header">
+      <form class="" method="post">
+        <input type="text" placeholder="Inizia la ricerca...">
+        <button type="submit" class="searchButton">
+          <i class="fa fa-search"></i>
+        </button>
+      </form>
+
+    </div>
+
+    <div class="user-header">
+      <div class="dropdown">
+        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          User
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#">Login</a>
+          <a class="dropdown-item" href="#">Register</a>
+          <a class="dropdown-item" href="#">Siuummm</a>
+          <a href="#">Ciao</a>
+        </div>
+      </div>
+    </div>
   </div>
+
 </header>
