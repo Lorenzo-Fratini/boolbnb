f966f6d9b2a2daf11db7344e253d6f640485c69f
B.C <benblog2000@gmail.com>
2021-06-16 15:25:50 +0200

    footer0.2 - debug

--- boolbnb/resources/views/components/footer.blade.php
+++ boolbnb/resources/views/components/footer.blade.php
@@ -1,13 +1,48 @@
 <footer>
     <div class="links">
-        <div class="container bg-dark">
-            <ul>
-                @for ($i = 0; $i < 5; $i++)
-                    <li>
-                        Lorem, ipsum dolor.
-                    </li>
-                @endfor
-            </ul>
+        <div class="container bg-dark" id="container_f">
+
+            {{-- debug --}}
+
+            <div class="row">
+                <ul>
+                    <h5>Hello</h5>
+                    @for ($i = 0; $i < 7; $i++)
+                        <li>
+                            Lorem, ipsum dolor.
+                        </li>
+                    @endfor
+                </ul>
+    
+                <ul>
+                    <h5>Hello</h5>
+                    @for ($i = 0; $i < 7; $i++)
+                        <li>
+                            Lorem, ipsum dolor.
+                        </li>
+                    @endfor
+                </ul>
+    
+                <ul>
+                    <h5>Hello</h5>
+                    @for ($i = 0; $i < 7; $i++)
+                        <li>
+                            Lorem, ipsum dolor.
+                        </li>
+                    @endfor
+                </ul>
+    
+                <ul>
+                    <h5>Hello</h5>
+                    @for ($i = 0; $i < 7; $i++)
+                        <li>
+                            Lorem, ipsum dolor.
+                        </li>
+                    @endfor
+                </ul>
+            </div>
+
+
         </div>
     </div>
 </footer>
