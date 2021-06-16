959460b88f957e668d5954520386c3244b3fffc5
B.C <benblog2000@gmail.com>
2021-06-16 14:24:57 +0200

    footer 0.2 - debug

--- boolbnb/resources/views/components/footer.blade.php
+++ boolbnb/resources/views/components/footer.blade.php
@@ -1,7 +1,13 @@
 <footer>
     <div class="links">
-        <div class="container f_margin bg-dark">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non porro placeat recusandae quos officiis dolorum odio sed nisi ea ad vel consequatur, nihil, perferendis deserunt deleniti quod excepturi corrupti amet.
+        <div class="container bg-dark">
+            <ul>
+                @for ($i = 0; $i < 5; $i++)
+                    <li>
+                        Lorem, ipsum dolor.
+                    </li>
+                @endfor
+            </ul>
         </div>
     </div>
 </footer>
