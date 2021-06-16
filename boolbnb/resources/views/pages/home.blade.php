bcfebe4f4b77e0616cfbbb6df399e08cde79bd5e
Alessandro <aletibo@gmail.com>
2021-06-16 15:57:09 +0200

    Update Header

--- boolbnb/resources/views/pages/home.blade.php
+++ boolbnb/resources/views/pages/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>CIAO</h1>
+  <div class="container-content">
+    <div class="margin-content">
+      <div class="text-content">
+        <h1>Esplora BoolBnB</h1>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-dintorni">
+    <h1>Esplora i dintorni</h1>
+
+    <div class="cities-dintorni">
+      <div class="city-box">
+
+      </div>
+    </div>
+  </div>
 @endsection
