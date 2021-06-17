09f2ed97029858329f92519204e88f7cf9501520
B.C <benblog2000@gmail.com>
2021-06-17 14:32:46 +0200

    creazione main footer e debug

---
+++ boolbnb/resources/views/pages/Search.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main-layout')
+
+@section('content')
+
+  <div class="container">
+
+    <div class="src-bar">
+
+      <input type="text" class="city-input" placeholder="city">
+
+      
+
+    </div>
+
+  </div>
+
+@endsection
