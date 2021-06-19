5d0505fcc377bae3c26d087be323bb1f1ea2c69c
paolotrimini <paolo.trimini@gmail.com>
2021-06-19 11:05:27 +0200

    dashboard css

--- boolbnb/resources/views/pages/dashboard.blade.php
+++ boolbnb/resources/views/pages/dashboard.blade.php
@@ -1,14 +1,36 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <div class="container text-center dash">
+    <div class="main-box">
         <div class="row">
-            <div class="col-12">
-                <h1> DASHBOARD:</h1>
-                
-                <p>Dettagli utente</p>
-                <p>Appartamenti</p>
-
+            <div class="box-side">
+                {{-- left side --}}
+                <div class="left-side">
+                    <p>Dettagli utente:</p>
+                </div>
+                {{-- right side --}}
+                <div class="right-side">
+                    <div class="box-title">
+                        <p>I miei appartamenti</p>
+                    </div>
+                    <div class="box-content">
+                        @foreach ($apartments as $apartment)
+                            <p>
+                                <i class="fas fa-home"></i>
+                                {{ $apartment -> title}} - {{ $apartment -> address}} - {{ $apartment -> city}}
+                                <a href="#">Edit</a>
+                                <a href="#">Delete</a>
+                            </p>
+                        @endforeach
+                    </div>
+                    <div class="box-apart">
+                        <p>Inserisci un nuovo appartamrto <a href="#">Nuovo appartamento</a></p>
+                    </div>
+                </div>
+                {{-- statistiche --}}
+                <div class="stat-side">
+                    <p>Statistiche</p>
+                </div>
             </div>
         </div>
     </div>
