9ddac6e703fac8fc5857dcef3a747824a193d632
B.C <benblog2000@gmail.com>
2021-06-20 18:38:16 +0200

    Home fix and debug

--- boolbnb/resources/views/pages/home.blade.php
+++ boolbnb/resources/views/pages/home.blade.php
@@ -29,7 +29,7 @@
       @foreach ($apartments as $apartment)
 
       
-      <div class="ap-lyt" style="background-image: url(storage/images/{{$apartment -> cover_image}})">
+      {{-- <div class="ap-lyt" style="background-image: url(storage/images/{{$apartment -> cover_image}})">
         
         <a href="{{ route('apartment.show', $apartment -> id) }}" class="prem-apart">
 
@@ -54,8 +54,38 @@
   
           </a>
   
-          </div>
+          </div> --}}
 
+          <div class="mini-container">
+
+            <img class="ap-img" src="storage/images/{{$apartment -> cover_image}}" alt="immagine principale appartamento {{ $apartment -> title }}">
+
+            <a href="{{ route('apartment.show', $apartment -> id) }}" class="prem-apart">
+
+              <div class="white-buble">
+                <h2>{{ $apartment -> title }}</h2>
+    
+              <div>
+    
+               <span>Numero di stanze:</span>
+                <span>{{ $apartment -> rooms_number }}</span>
+    
+              </div>
+            
+              <div class="details">
+    
+               <i class="fas fa-bed"></i>
+               <span>{{ $apartment -> beds_number }}</span>
+      
+                <i class="fas fa-toilet"></i>
+                <span>{{ $apartment -> bathrooms_number }}</span>
+    
+              </div>
+              </div>
+    
+            </a>
+
+          </div>
           
       @endforeach
 
