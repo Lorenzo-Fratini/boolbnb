a3d25975948de197ee1a857227ccb89e26bb046b
paolotrimini <paolo.trimini@gmail.com>
2021-06-21 17:08:29 +0200

    search vue

--- boolbnb/resources/views/pages/apartmentSearch.blade.php
+++ boolbnb/resources/views/pages/apartmentSearch.blade.php
@@ -2,11 +2,18 @@
 
 @section('content')
     <main>
-        <div style="margin-top: 100px;">
-            @foreach ($apartments as $apartment)
-                <p>{{ $apartment -> title }}</p>
-                <p>{{ $apartment -> city }}</p>
-            @endforeach
+        <div id="search">
+            <div style="margin-top: 100px;">
+                @foreach ($apartments as $apartment)
+                    <p>{{ $apartment -> title }}</p>
+                    <p>{{ $apartment -> city }}</p>
+                @endforeach
+            </div>
+    
+            {{-- ricerca avanzata Vue --}}
+            <div>
+                <search-component></search-component>
+            </div>
         </div>
     </main>
 @endsection
