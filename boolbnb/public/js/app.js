74c3bf4eb1bf4635b3aa5ced0fe443067f9da1b5
B.C <benblog2000@gmail.com>
2021-06-24 00:37:01 +0200

    fix pagina dettaglio

--- boolbnb/public/js/app.js
+++ boolbnb/public/js/app.js
@@ -49891,8 +49891,6 @@ files.keys().map(function (key) {
 
 /* document.addEventListener('DOMContentLoaded', () => { */
 
-// }); */
-
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
@@ -50027,8 +50025,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Laravel\boolbnb\boolbnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Laravel\boolbnb\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Git\boolbnb\boolbnb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Git\boolbnb\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
