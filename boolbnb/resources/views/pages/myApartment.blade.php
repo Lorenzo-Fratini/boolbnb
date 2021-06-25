798ac01353e5af6826e0ee4a3f6f397a846b6749
Diego Barbaresco <diego.bar@live.it>
2021-06-25 16:53:11 +0200

    dati per chartjs

--- boolbnb/resources/views/pages/myApartment.blade.php
+++ boolbnb/resources/views/pages/myApartment.blade.php
@@ -61,7 +61,7 @@
 
    <div class="my-apartment">
 
-        <div class="stats">
+        <div class="stats" id="stats">
             <div>
 
                 <h1>Statistiche</h1>
@@ -98,41 +98,109 @@
 </div>
 
 <script>
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
+
+    new Vue ({
+        el: '#stats',
         data: {
-            labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
-            datasets: [{
-                label: '# of Visits',
-                data: [12, 19, 10, 5, 2, 3, 5, 6, 7, 3, 10, 20],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
+            messages: {},
+            statistics: {},
+            months: '',
+            years: '',
         },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+        methods: {
+
+            chartCreate: function (){
+
+                let ctx = document.getElementById('myChart').getContext('2d');
+                let myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
+                        datasets: [{
+                            label: '# of Visits',
+                            data: this.statistics.year.month,
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+            },
+
+        },
+
+        mounted(){
+
+            axios.post('/api/getStatistics/' + {{$apartment -> id}})
+            .then(res => {
+
+                this.statistics = res.data;
+                let statistics = [];
+
+                for (let x = 0; x < 12; x++) {
+
+                    let month = (x + 1).toString();
+                    
+                    if (this.statistics['2021'][month]) {
+
+                        statistics.push(this.statistics['2021'][month].length);
+                    } else {
+
+                        statistics.push(0);
+                    }
                 }
-            }
+
+                this.statistics = statistics;
+
+                // console.log(this.statistics);
+            });
+
+            axios.post('/api/getMessages/' + {{$apartment -> id}})
+            .then(res => {
+
+                this.messages = res.data;
+                let messages = [];
+
+                for (let x = 0; x < 12; x++) {
+
+                    let month = (x + 1).toString();
+                    
+                    if (this.messages['2021'][month]) {
+
+                        messages.push(this.messages['2021'][month].length);
+                    } else {
+
+                        messages.push(0);
+                    }
+                }
+
+                this.messages = messages;
+
+                // console.log(this.messages);
+            });
         }
-    });
+    })
 
 </script>
 @endsection
