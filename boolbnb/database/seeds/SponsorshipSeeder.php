12335f03422fae2e49a7d274902c5700dce6c3ab
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-06-25 12:56:33 +0200

    DB correction

--- boolbnb/database/seeds/SponsorshipSeeder.php
+++ boolbnb/database/seeds/SponsorshipSeeder.php
@@ -15,30 +15,23 @@ class SponsorshipSeeder extends Seeder
     public function run()
     {
 
-        factory(Sponsorship::class, 3) -> create();
-        /*     -> each(function($sponsorship) {
+        $sponsorships = [
+            [
+                'price' => 2.99,
+                'duration' => 24
+            ],
+            [
+                'price' => 5.99,
+                'duration' => 72
+            ],
+            [
+                'price' => 9.99,
+                'duration' => 144
+            ]
+        ];
 
-            $startDates = [
-                '2021-01-03',
-                date("Y-m-d H:i:s", time())
-            ];
-
-            $startDate = $startDates[rand(0, 1)];
-
-            if ($sponsorship -> id == 1) {
-
-                $endDate = date("Y-m-d H:i:s", strtotime('+24 hours', strtotime($startDate)));
-            } else if ($sponsorship -> id == 2) {
-
-                $endDate = date("Y-m-d H:i:s", strtotime('+48 hours', strtotime($startDate)));
-            } else {
-
-                $endDate = date("Y-m-d H:i:s", strtotime('+144 hours', strtotime($startDate)));
-            }
-                
-            $apartment = Apartment::inRandomOrder() -> first();
-            $sponsorship -> apartments() -> attach($apartment, ['start_date' => $startDate, 'end_date' => $endDate]); 
-            $sponsorship -> save();
-        });*/
+        foreach ($sponsorships as $key => $sponsorship) {
+            Sponsorship::create($sponsorship);
+        }
     }
 }
