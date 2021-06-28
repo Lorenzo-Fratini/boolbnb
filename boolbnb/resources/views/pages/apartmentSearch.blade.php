c28fb85647023cc7374e12eb850dbc6bb3c557d3
Diego Barbaresco <diego.bar@live.it>
2021-06-28 11:36:12 +0200

    global fix

--- boolbnb/resources/views/pages/apartmentSearch.blade.php
+++ boolbnb/resources/views/pages/apartmentSearch.blade.php
@@ -65,161 +65,141 @@
 
             methods: {
 
-
                 filterApartments: function() {
 
-                    const searchString = new URL(location.href).searchParams.get('searchString');
-                    let bedsRooms = [];
-                    bedsRooms.push(this.beds, this.rooms);
+                const searchString = new URL(location.href).searchParams.get('searchString');
+                let bedsRooms = [];
+                bedsRooms.push(this.beds, this.rooms);
+                
+
+                if (this.filterServices.length > 0) {
+                    
+                    axios.post('/api/filterApartments/' + searchString + '/' + this.filterServices + '/' + bedsRooms) 
+                    .then(res => {
+
+                        this.currentApartments = res.data;
+                        //console.log(res.data);
+                        this.getMap(searchString)
+                    });
+
+
+                } else {
+
+                    let filterServices = 'noServices';
+
+                    axios.post('/api/filterApartments/' + searchString + '/' + filterServices + '/' + bedsRooms) 
+                    .then(res => {
+
+                        this.currentApartments = res.data;
+                        //console.log(res.data);
+                        this.getMap(searchString)
+                    });
+
+                }
+
+                
+                },
+
+                getMap: function(searchString){
+
+                console.log('hello');
+
+                // TOM TOM 
+                axios.get('https://api.tomtom.com/search/2/geocode/%20' + searchString + '%20it.JSON?key=e221oCcENGoXZRDyweSTg7PnYGiEXO82', {headers:''})
+                .then(res => {
+
+                    this.testtomtomapi = res.data.results[0];
+
+                    this.lat = this.testtomtomapi.position.lat;
+                    this.lon = this.testtomtomapi.position.lon;
+                
+                    
+                    // Tom Tom Map
+
+                    //console.log(this.lat, this.lon);
+
+                    var APIKEY = "XPOiPra9khmu2grECjX15gw5Cdy98fSX"
+                    var CITY = [this.lon, this.lat] // [longitudine, latitudine]
+                    //var ROMA1 = [12.48536, 41.88697]
+                
+                
+                    var map = tt.map({
+                        key: APIKEY,
+                        container: 'mymap',
+                        center: CITY,
+                        zoom: 11,
+                        style: 'tomtom://vector/1/basic-main'
+                    });
+
+
+                    // Bandiere + popup Appartamenti su mappa
+                    for(i=0; i<this.currentApartments.length; i++){
+
+                        var appLatLon = [ this.currentApartments[i].longitude, this.currentApartments[i].latitude]
+                        var appTitle = [this.currentApartments[i].title]
+                        var appAddress = [this.currentApartments[i].address]
+
+                        //console.log(this.allApartments);
+                        console.log(this.currentApartments);
+
+                        var marker1 = new tt.Marker().setLngLat(appLatLon).addTo(map);
+
+                        var popup = new tt.Popup({offset: popupOffsets}).setHTML("<strong>" + this.currentApartments[i].title + "</strong>" + "<br>" + this.currentApartments[i].address);
+                        marker1.setPopup(popup).togglePopup();
+
+                    }
+
+                    // bandiere su mappa
+                    // var marker = new tt.Marker().setLngLat(CITY).addTo(map);
+                
+
+                    var popupOffsets = {
+                        top: [0, 0],
+                        bottom: [0, -70],
+                        'bottom-right': [0, -70],
+                        'bottom-left': [0, -70],
+                        left: [25, -35],
+                        right: [-25, -35]
+                    }
+                    
+                    // popup nomi appartamenti
+                    // var popup = new tt.Popup({offset: popupOffsets}).setHTML("CITY");
+                    // marker.setPopup(popup).togglePopup();
                     
 
-                    if (this.filterServices.length > 0) {
-                        
-                        axios.post('/api/filterApartments/' + searchString + '/' + this.filterServices + '/' + bedsRooms) 
-                        .then(res => {
 
-                            this.currentApartments = res.data;
-                            //console.log(res.data);
-                        });
+                    //controlli mappa    
+                    map.addControl(new tt.NavigationControl());
 
-                    } else {
+                });
 
-                        let filterServices = 'noServices';
-
-                        axios.post('/api/filterApartments/' + searchString + '/' + filterServices + '/' + bedsRooms) 
-                        .then(res => {
-
-                            this.currentApartments = res.data;
-                            //console.log(res.data);
-                        });
-                    }
-                },
+                }
 
             },
 
             mounted() {
-                
 
-                const searchString = new URL(location.href).searchParams.get('searchString');
 
-                axios.get('/api/getApartments/' + searchString)
-                    .then(res => {
+            const searchString = new URL(location.href).searchParams.get('searchString');
 
-                        this.allApartments = res.data;
-                        this.currentApartments = this.allApartments;
-                        // console.log(this.currentApartments, this.allApartments);
+            axios.get('/api/getApartments/' + searchString)
+                .then(res => {
 
-                            // TOM TOM 
-                            axios.get('https://api.tomtom.com/search/2/geocode/%20' + searchString + '%20it.JSON?key=e221oCcENGoXZRDyweSTg7PnYGiEXO82', {headers: ''})
-                            .then(res => {
+                    this.allApartments = res.data;
+                    this.currentApartments = this.allApartments;
+                    // console.log(this.currentApartments, this.allApartments);
 
-                                this.testtomtomapi = res.data.results[0];
+                    this.getMap(searchString)
 
-                                this.lat = this.testtomtomapi.position.lat;
-                                this.lon = this.testtomtomapi.position.lon;
-                                //console.log(this.currentApartments);
-
-                                // console.log(this.lat, this.lon);
-                                // console.log(this.testtomtomapi);
-                                
-                                // Tom Tom Map
-
-                                //console.log(this.lat, this.lon);
-
-                                var APIKEY = "XPOiPra9khmu2grECjX15gw5Cdy98fSX"
-                                var CITY = [this.lon, this.lat] // [longitudine, latitudine]
-                                //var ROMA1 = [12.48536, 41.88697]
-                            
-                            
-                                var map = tt.map({
-                                    key: APIKEY,
-                                    container: 'mymap',
-                                    center: CITY,
-                                    zoom: 11,
-                                    style: 'tomtom://vector/1/basic-main'
-                                });
-
-                                
-
-                                // // Bandiere + popup Appartamenti su mappa
-                                for(i=0; i<this.currentApartments.length; i++){
-
-                                    var appLatLon = [ this.currentApartments[i].longitude, this.currentApartments[i].latitude]
-                                    var appTitle = [this.currentApartments[i].title]
-                                    var appAddress = [this.currentApartments[i].address]
-
-                                    //console.log(this.allApartments);
-                                    console.log(this.currentApartments);
-
-                                    var marker1 = new tt.Marker().setLngLat(appLatLon).addTo(map);
-
-                                    var popup = new tt.Popup({offset: popupOffsets}).setHTML("<strong>" + this.currentApartments[i].title + "</strong>" + "<br>" + this.currentApartments[i].address);
-                                    marker1.setPopup(popup).togglePopup();
-
-                                }
-
-                                // bandiere su mappa
-                                var marker = new tt.Marker().setLngLat(CITY).addTo(map);
-                            
-
-                                var popupOffsets = {
-                                    top: [0, 0],
-                                    bottom: [0, -70],
-                                    'bottom-right': [0, -70],
-                                    'bottom-left': [0, -70],
-                                    left: [25, -35],
-                                    right: [-25, -35]
-                                }
-                                
-                                // nomi appartamenti su bandiere
-                                var popup = new tt.Popup({offset: popupOffsets}).setHTML("CITY");
-                                marker.setPopup(popup).togglePopup();
-                               
-                    
-
-                                //controlli mappa    
-                                map.addControl(new tt.NavigationControl());
-
-                            });
-
-                    });
-
-                axios.get('/api/getServices/')
-                    .then(res => {
-
-                        this.allServices = res.data;
                 });
 
-                // axios.get('https://api.tomtom.com/search/2/geocode/%20' + searchString + '%20it.JSON?key=e221oCcENGoXZRDyweSTg7PnYGiEXO82', {headers: ''})
-                //     .then(res => {
+            axios.get('/api/getServices/')
+                .then(res => {
 
-                //         this.testtomtomapi = res.data.results[0];
+                    this.allServices = res.data;
+            });
 
-                //         this.lat = this.testtomtomapi.position.lat;
-                //         this.lon = this.testtomtomapi.position.lon;
 
-                //         // console.log(this.lat, this.lon);
-
-                //         // console.log(this.testtomtomapi);
-                        
-                //         // Tom Tom Map
-
-                //         console.log(this.lat, this.lon);
-
-                //         var APIKEY = "XPOiPra9khmu2grECjX15gw5Cdy98fSX"
-                //         var CITY = [this.lon, this.lat] // [longitudine, latitudine]
-
-                //         var map = tt.map({
-                //             key: APIKEY,
-                //             container: 'mymap',
-                //             center: CITY,
-                //             zoom: 10,
-                //             style: 'tomtom://vector/1/basic-main'
-                //         });
-
-                // });
-
-                
 
             }, // fine Mounted()
 
