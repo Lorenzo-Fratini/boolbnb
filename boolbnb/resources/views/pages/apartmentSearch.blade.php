ec8c128513419581ec09084d4863f493ac661129
B.C <benblog2000@gmail.com>
2021-07-02 11:40:06 +0200

    General layout correction

--- boolbnb/resources/views/pages/apartmentSearch.blade.php
+++ boolbnb/resources/views/pages/apartmentSearch.blade.php
@@ -4,9 +4,19 @@
     <main>
             <div id="search">
 
+            {{-- button menu' --}}
+            <div id="menu">
+
+                <button>
+                    <i class="far fa-compass"></i>
+                    <span>FILTRI</span>
+                </button>
+
+            </div>
+
             {{-- servizi --}}
             <div class="box-service">
-                <div style="display:flex">
+                <div class="dis-flex">
                     <div v-for="service in allServices" class="my-services">
                         <input v-on:change="filterApartments" type="checkbox" :name="service.name" :value="service.id"
                             v-model="filterServices">
@@ -14,16 +24,30 @@
                     </div>
                 </div>
 
-                <div>
-                    <label for="rooms">Rooms</label>
-                    <input v-on:change="filterApartments" type="number" name="rooms" id="rooms" v-model="rooms"
-                        onkeydown="return false" min="1">
-                    <label for="beds">Beds</label>
-                    <input v-on:change="filterApartments" type="number" name="beds" id="beds" v-model="beds"
-                        onkeydown="return false" min="1">
+                <div class="dis-flex filter">
+                    <div class="center">
+
+                        <label for="rooms">Camere</label>
+                        <input v-on:change="filterApartments" type="number" name="rooms" id="rooms" v-model="rooms"
+                            onkeydown="return false" min="1">   
+
+                    </div>
+                    
+                    <div class="center">
+
+                        <label for="beds">Letti</label>
+                        <input v-on:change="filterApartments" type="number" name="beds" id="beds" v-model="beds"
+                            onkeydown="return false" min="1">
+                            
+                    </div>
+                    
+                    <div class="center">
+
                         <label for="radius">Raggio</label>
                         <input v-on:change="filterApartments" type="range" id="radius" name="radius" v-model="radius"
                         min="0" max="20">
+
+                    </div>
                 </div>
             </div>
 
@@ -31,8 +55,8 @@
             <div class="box-app-map">
                 <div class="my-apartments">
                     <div v-for="apartment in currentApartments" v-if="apartment.visible == 0" class="spc-apart">
-{{--                         <a :href="'/apartment/' + apartment.id">
- --}}                        <hr>
+                        <hr>
+                         <a :href="'/apartment/' + apartment.id">
                             <div id="img-box">
                                 <img :src="'/storage/images/' + apartment.cover_image">
                             </div>
@@ -45,11 +69,11 @@
 
                                 </h3>
 
-                                Address: @{{ apartment.address}}
+                                Indirizzo: @{{ apartment.address}}
 
                             </div>
-{{--                         </a>
- --}}                    </div>
+                         </a>
+                    </div>
                     <hr>
                 </div>
                 {{-- tom tom --}}
