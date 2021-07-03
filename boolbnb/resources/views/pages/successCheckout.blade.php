6c858ed97b75892a7d5ccd8f0fc5d5953e7ee408
B.C <benblog2000@gmail.com>
2021-07-03 09:36:24 +0200

    checkout layout correction

--- boolbnb/resources/views/pages/successCheckout.blade.php
+++ boolbnb/resources/views/pages/successCheckout.blade.php
@@ -3,11 +3,25 @@
 @section('content')
 
     <main>
-        <h1>{{ $apartment -> title }}</h1>
-        <h3>Pacchetto: {{ $sponsorship -> duration }}h - {{ $sponsorship -> price }}€</h3>
-        <p>Data inizio: {{ $startDate }} | Data fine: {{ $endDate }}</p>
-    
-        <a href="{{ route('dashboard', $apartment -> user_id) }}">Dashboard</a>
+        
+        <div class="title">
+            <h1>GRAZIE PER L'ACQUISTO</h1>
+        </div>
+
+        <div class="card-container">
+
+            <div class="card">
+
+                <h1 class="elm">{{ $apartment -> title }}</h1>
+                <h3 class="elm">Pacchetto: {{ $sponsorship -> duration }}h - {{ $sponsorship -> price }}€</h3>
+                <p class="elm">Data inizio: {{ $startDate }} | Data fine: {{ $endDate }}</p>
+            
+                <a class="elm" href="{{ route('dashboard', $apartment -> user_id) }}">Dashboard</a>
+
+            </div>
+
+        </div>
+        
     </main>
 
 
